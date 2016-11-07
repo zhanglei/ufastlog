fee6275785d917700b3c53feeaca0432be5bc7c2
ZhiFeng Hu <hufeng1987@gmail.com>
2016-11-07 11:20:27 +0800

    Try to send log msg to upstream udp server
    Signed-off-by: ZhiFeng Hu <hufeng1987@gmail.com>

--- client/php/test.php
+++ client/php/test.php
@@ -3,8 +3,12 @@ class ufastlog
 {
     public static $logprefix;
     public static $logrequestid;
+    public static $upstreamAddr;
+    public static $upstreamPort;
     public static function setConfig($config = [])
     {
+        static::$upstreamAddr = (array_keys('upstreamaddr', $config)) ? $config['upstreamaddr'] : 'udp://127.0.0.1';
+        static::$upstreamPort = (array_keys('upstreamport', $config)) ? $config['upstreamport'] : '1043';
         static::$logprefix = (array_key_exists('prefix', $config)) ? $config['prefix'] : 'log_2016_11';
         static::$logrequestid = (array_key_exists('requestid', $config)) ? $config['requestid'] : str_replace(".", "", microtime(true)) . rand(1000, 9999);
     }
@@ -21,8 +25,17 @@ class ufastlog
     }
     public static function send($msg)
     {
-        var_dump($msg);
-        var_dump($msgArr = unpack("a64logprefix/a64logrequestid/a*msg", $msg));
+        $fp = fsockopen(static::$config['upstreamaddr'], static::$config['upstreamport'], $errno, $errmsg);
+        if (!$fp) {
+            echo "ERROR: " . $errno . ": " . $errmsg . PHP_EOL;
+            return ;
+        } else {
+            fwrite($fp, $msg);
+            return fread($fp, 64);
+        }
+        fclose($fp);
+        //var_dump($msg);
+        //var_dump($msgArr = unpack("a64logprefix/a64logrequestid/a*msg", $msg));
     }
 }
 
